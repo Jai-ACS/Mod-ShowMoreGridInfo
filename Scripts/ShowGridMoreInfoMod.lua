846f3079f7f2bb227b20bb97818352a7ec41bd0e
Jai <jyai.san+acs@gmail.com>
2025-11-23 19:18:07 +0800

    Update ShowGridMoreInfoMod.lua

--- Scripts/ShowGridMoreInfoMod.lua
+++ Scripts/ShowGridMoreInfoMod.lua
@@ -13,10 +13,10 @@ function ShowGridMoreInfoMod:OnInit()
 end
 
 function ShowGridMoreInfoMod:OnRender(_)
-	self:newShowGridInfo()
+	self:ShowGridInfo()
 end
 
-function ShowGridMoreInfoMod:newShowGridInfo()
+function ShowGridMoreInfoMod:ShowGridInfo()
 	if CS.UI_WorldLayer.Instance == nil then
 		return
 	end
@@ -31,7 +31,7 @@ function ShowGridMoreInfoMod:newShowGridInfo()
 	
 	if self.bHasResizeTextArea ~= true then
 		CS.Wnd_GameMain.Instance.UIInfo.m_n32.UBBEnabled = true
-		CS.Wnd_GameMain.Instance.UIInfo.m_n32:SetSize(150, 175)
+		CS.Wnd_GameMain.Instance.UIInfo.m_n32:SetSize(200, 175)
 		self.bHasResizeTextArea = true
 	end
 	
@@ -65,7 +65,7 @@ function ShowGridMoreInfoMod:newShowGridInfo()
 	if fLingAddion == 0 then
 		strLingAddion = "无"
 	elseif fToMaxLingAddionTime == 0 then
-		strLingAddion = string.format("%.2f[color=#00FF00](已达到理论值)[/color]", fLingAddionInFact)
+		strLingAddion = string.format("%.2f[color=#00FF00]" .. XT("（最高值）" .. "[/color]", fLingAddionInFact)
 	else
 		strLingAddion = string.format("%.2f[color=#FF0000](%s后到%.2f)[/color]", fLingAddionInFact, self:GameTime2Str(fToMaxLingAddionTime), fLingAddion)
 	end
@@ -134,56 +134,6 @@ function ShowGridMoreInfoMod:newShowGridInfo()
 			strMsg3, strMsg5, strMsg7)
 end
 
-function ShowGridMoreInfoMod:ShowGridInfo()
-	if CS.UI_WorldLayer.Instance == nil then
-		return
-	end
-
-	if GridMgr == nil then
-		return
-	end
-
-	if CS.Wnd_GameMain.Instance.UIInfo == nil then
-		return
-	end
-
-	if self.bHasResizeTextArea ~= true then
-		CS.Wnd_GameMain.Instance.UIInfo.m_n32.UBBEnabled = true
-		CS.Wnd_GameMain.Instance.UIInfo.m_n32:SetSize(150, 175)
-		self.bHasResizeTextArea = true
-	end
-
-	-- local nCurMouseKey = CS.UI_WorldLayer.Instance.MouseGridKey
-	-- if nCurMouseKey == self.nLastMouseKey and self.bForceUpdate ~= true then
-		-- return
-	-- end
-	-- self.nLastMouseKey = nCurMouseKey
-	local curMousePosition = CS.UI_WorldLayer.Instance.MousePostion
-	if curMousePosition == self.lastMousePosition and self.bForceUpdate ~= true then
-		return
-	end
-	self.lastMousePosition = curMousePosition
-	self.bForceUpdate = nil
-
-	-- if not GridMgr:KeyVaild(nCurMouseKey) then
-		-- return
-	-- end
-	
-	local nCurMouseKey = GridMgr:Pos2Grid(curMousePosition, true)
-
-	if CS.Wnd_GameMain.Instance.openFengshui then
-		local EArray = Map:GetElement(nCurMouseKey)
-		local EPArray = Map:GetElementProportion(nCurMouseKey)
-		CS.Wnd_GameMain.Instance.UIInfo.m_n32.text = string.format("[color=#FFEB68]金  %05.2f  %02.0f%%[/color]\n[color=#78C84E]木  %05.2f  %02.0f%%[/color]\n[color=#81C1F5]水  %05.2f  %02.0f%%[/color]\n[color=#DA494E]火  %05.2f  %02.0f%%[/color]\n[color=#986B39]土  %05.2f  %02.0f%%[/color]",
-				EArray[1], EPArray[1] * 100,
-				EArray[2], EPArray[2] * 100,
-				EArray[3], EPArray[3] * 100,
-				EArray[4], EPArray[4] * 100,
-				EArray[5], EPArray[5] * 100)
-		return
-	end
-end
-
 function ShowGridMoreInfoMod:OnWindowEvent(pThing, pObjs)
 	local pWnd = pObjs[0]
 	local iArg = pObjs[1]
@@ -192,4 +142,30 @@ function ShowGridMoreInfoMod:OnWindowEvent(pThing, pObjs)
 		pWnd.UIInfo.m_friendpontvalue.visible = true
 		pWnd.UIInfo.m_friendpontvalue.y = pWnd.UIInfo.m_itemvalue.y - pWnd.UIInfo.m_friendpontvalue.actualHeight
 	end
+end
+
+function ShowGridMoreInfoMod:GetValueByMap(tbMap, Key)
+	local Value
+	for k, v in pairs(tbMap) do
+		if Key >= k then
+			return v
+		end
+		Value = v
+	end
+	return Value
+end
+
+-- 游戏的时间转成便于现实阅读的时间格式
+function ShowGridMoreInfoMod:GameTime2Str(fGameTime)
+	if fGameTime > 600 then
+		return string.format("%.2f" .. XT("天"), fGameTime / 600)
+	end
+	local fReallyTime = fGameTime / 600 * 24 * 3600
+	local nHour = math.modf(fReallyTime / 3600)
+	local nMin = math.modf(fReallyTime % 3600 / 60)
+	return string.format("%02d:%02d", nHour, nMin)
+end
+
+function ShowGridMoreInfoMod:BindFertilityColor(strFertility)
+	return string.format("[coolor=#%s]%s[color]", self.tbFertilityColor[strFertility], strFertility)
 end
